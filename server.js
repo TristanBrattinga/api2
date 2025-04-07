43f20a2511229f9a0877b32052958f72082b1b7f
TristanBrattinga <tristan.brattinga@gmail.com>
2025-04-07 22:02:54 +0200

    added liquid setup

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 import { App } from '@tinyhttp/app'
+import { Liquid } from 'liquidjs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = new App()
 
-app.use((req, res) => void res.send('Hello world!'))
+const engine = new Liquid({
+	root: path.join(__dirname, 'views'),
+	extname: '.liquid'
+})
+
+app.get('/', async (req, res) => {
+	const html = await engine.renderFile('index')
+	res.send(html)
+})
 
 app.listen(3000, () => console.log('Started on http://localhost:3000'))
